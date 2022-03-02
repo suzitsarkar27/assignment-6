d3c104f0ba8a3559a6081ab5a243083cd844d8dc
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-03-02 16:34:49 +0600

    phone-hunter

--- phone.js
+++ phone.js
@@ -7,7 +7,9 @@ const phoneEvent = () => {
     document.getElementById('error-massage').innerHTML = '';
     document.getElementById('error-massage').innerHTML = '';
     document.getElementById('phone-datlice').innerHTML = '';
-  
+
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
     if (inputValue == "" || inputValue == 0) {
       const errpr=  document.getElementById('error-massage').innerHTML = `
         <h2>Sorry not found of infro plasse try agine</h2>
@@ -15,7 +17,7 @@ const phoneEvent = () => {
         input.value = "";
         errpr.innerHTML = '';
 
-    } else if (inputValue>20) {
+    } else if (inputValue>20 ) {
         document.getElementById('error-massage').innerHTML = `
         <h2> Sorry not a data trye aging</h2>
        `
@@ -32,19 +34,23 @@ const phoneEvent = () => {
 }
 
 const displayPhone = phones => {
+   
+    
     const container = document.getElementById('container');
+    container.innerHTML = '';
     if (phones.length == 0) {
         const errpr=  document.getElementById('error-massage').innerHTML = `
         <h2>Sorry not found of infro plasse try agine</h2>
        `
         input.value = "";
         errpr.innerHTML = '';
-    }
+    } 
     const phone = phones.slice(0, 20);
     phone.forEach(element => {
         // console.log(element)
         const div = document.createElement('div');
-        div.className=('col-col-md-6 col-lg-4 mb-3 p-5 m-auto')
+        div.innerHTML = '';
+        div.className=('col- col-md-6 col-lg-4 mb-3 p-5 m-auto')
         div.innerHTML = `
           <div class="card text text-center">
               <div>
@@ -60,38 +66,42 @@ const displayPhone = phones => {
          `
          container.appendChild(div)
     });
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'none';
 }
  
 const detalic = (phoneDatlic) => {
     const url = (`https://openapi.programming-hero.com/api/phone/${phoneDatlic}`)
      fetch(url)
     .then(res => res.json())
-    .then(data=>displayInfro(data.data))
+        .then(data => displayInfro(data.data))
+        const spinner = document.getElementById('spinner');
+        spinner.style.display = 'block';
 }  
 const displayInfro = infroData => {
     const phoneDatlic = document.getElementById('phone-datlice')
-   
+   console.log(infroData.others)
     const div = document.createElement('div');
     document.getElementById('phone-datlice').innerHTML = '';
+    div.className=('row border border-primary shadow-lg p-3 mb-5 bg-body rounded')
     div.innerHTML = `
-    <div class="card text-center">
-    <div>
-        <img class="mt-3 w-50" src="${infroData.image}" alt="">
-    </div>
-       <h2>Name:${infroData.name}</h2>
-       <h3>Release Date:${infroData.releaseDate}</h3>
-       <h4>Memory:${infroData.mainFeatures. memory}</h4>
-       <h5>Bluetooth:${infroData.others.Bluetooth}</h5>
-       <h5>USB:${infroData.others.USB}</h5>
-       <h5>WLAN:${infroData.others.WLAN}</h5>
-       <h5>Sensors:${infroData.mainFeatures.sensors}</h5>
-       
-   
-      
-   <div>
-      <button class="btn btn-success mb-4">All Data</button>
-    </div>
-</div>
-    `
+     <div class="col-md-6">
+     <img class="mt-3" src="${infroData.image}" alt="">
+     </div>
+     <div class="col-md-6 w-70">
+         <div class="text-center">
+            <h2>Name:${infroData.name}</h2>
+            <h3>Release Date:${infroData.releaseDate}</h3>
+            <h5>Others:
+              ${infroData.others.Bluetooth}
+            </h5>
+            <p>Sensors:${infroData.mainFeatures.sensors}</p>
+     </div>       
+            `
     phoneDatlic.appendChild(div)
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'none';
+
 }
+
+
